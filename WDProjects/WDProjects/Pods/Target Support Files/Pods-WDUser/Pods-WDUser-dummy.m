4161ad62d41c4e253518a03a49afc70fd5d6c9f1
weidong <jiangweidong@hetao101.com>
2020-06-20 17:30:20 +0800

    编写pod文件支持多个工程

---
+++ WDProjects/WDProjects/Pods/Target Support Files/Pods-WDUser/Pods-WDUser-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WDUser : NSObject
+@end
+@implementation PodsDummy_Pods_WDUser
+@end
--- WDProjects/WDProjects/WDUser/WDUser/WDUserLog.m
+++ WDProjects/WDProjects/WDUser/WDUser/WDUserLog.m
@@ -7,6 +7,7 @@
 //
 
 #import "WDUserLog.h"
+#import <Masonry.h>
 
 @implementation WDUserLog
 - (void)userLog {
